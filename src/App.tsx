24d3a2d11dd88fb9cf00f8bc34202bbd1115b343
Pearla <pearla@Pearlas-MacBook-Pro.local>
2025-08-20 17:10:27 -0500

    fix: import App as default in index.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,19 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
 import LandingPage from './pages/LandingPage';
 import ChatLanderPage from './pages/ChatLanderPage';
 import CreateFantasyPage from './pages/CreateFantasyPage';
-export function App() {
-  return <BrowserRouter>
+
+export default function App() {
+  return (
+    <BrowserRouter basename={import.meta.env.BASE_URL}>
       <Routes>
         <Route path="/" element={<LandingPage />} />
         <Route path="/lander/:id" element={<ChatLanderPage />} />
         <Route path="/create-fantasy" element={<CreateFantasyPage />} />
       </Routes>
-    </BrowserRouter>;
+    </BrowserRouter>
+  );
 }
+
